b1cac0891425e93382ef7f1359a41c7cec822e2d
gerardor <gerardor.com@gmail.com>
2015-02-25 17:53:56 -0200

    se agregan rutas y paginas publicas

---
+++ config/database.js
@@ -0,0 +1,6 @@
+//config/database.js
+
+module.exports = {
+	'url' : 'mongodb://gerardor:chalaman@ds039431.mongolab.com:39431/data4viz' //looks like
+	//  mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
--- server.js
+++ server.js
@@ -15,6 +15,14 @@ var cookieParser	= require('cookie-parser');
 var bodyParser		= require('body-parser');
 var session			= require('express-session');
 
+var configDB		= require('./config/database.js');
+
+//configuration --------------
+mongoose.connect(configDB.url);//connect to db
+
+//require('./config/passport')(passport); //pass passport for configuration
+
+
 
 //set up our express application
 app.use(morgan('dev'));//log every request to the console
@@ -25,7 +33,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');//set up ejs for templating
 
 //required for passport
-app.use(sessions({ secret: 'ilovescotch' }));//session secret
+app.use(session({ secret: 'ilovescotch' }));//session secret
 app.use(passport.initialize());
 app.use(passport.session()); //persistent login session
 app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+//routes ----------
+require('./app/routes.js')(app, passport); //load our routes pass in our app and fully configured passport
+
+//launch ------------
+app.listen(port);
+console.log('The magic happens on port ' + port);
